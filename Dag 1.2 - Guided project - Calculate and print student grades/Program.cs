d7b7d29bd4f5fdef0a20d46b7c54a3aa8849a4bb
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-19 16:06:36 +0100

    a

--- Dag 1.2 - Guided project - Calculate and print student grades/Program.cs
+++ Dag 1.2 - Guided project - Calculate and print student grades/Program.cs
@@ -1,4 +1,6 @@
 ﻿// initialize variables - graded assignments 
+using System.Security;
+
 int currentAssignments = 5;
 
 int sophia1 = 93;
@@ -13,21 +15,21 @@ int nicolas2 = 83;
 int nicolas3 = 82;
 int nicolas4 = 88;
 int nicolas5 = 85;
-decimal nicolasAverage = (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / 5m;
+decimal nicolasAverage = (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / (decimal)currentAssignments;
 
 int zahirah1 = 84;
 int zahirah2 = 96;
 int zahirah3 = 73;
 int zahirah4 = 85;
 int zahirah5 = 79;
-decimal zahirahAverage = (zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / 5m;
+decimal zahirahAverage = (zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / (decimal)currentAssignments;
 
 int jeong1 = 90;
 int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
-decimal jeongAverage = (jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / 5m;
+decimal jeongAverage = (jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / (decimal)currentAssignments;
 
 Console.WriteLine($"Sophia: {sophia1}, {sophia2}, {sophia3}, {sophia4}, {sophia5}\n");
 Console.WriteLine($"Nicolas: {nicolas1}, {nicolas2}, {nicolas3}, {nicolas4}, {nicolas5}\n");
--- Dag 1.3 - Guided project - Calculate final GPA/Program.cs
+++ Dag 1.3 - Guided project - Calculate final GPA/Program.cs
@@ -18,3 +18,18 @@ int course4Grade = 3;
 int course5Grade = 4;
 
 decimal engSum = course1Grade * course1Credit;
+decimal algSum = course2Grade * course2Credit;
+decimal bioSum = course3Grade * course3Credit;
+decimal comSum = course4Grade* course4Credit;
+decimal psySum = course5Grade* course5Credit;
+
+decimal gpa = (((decimal)course1Grade * course1Credit) + (course2Grade * course2Credit) + (course3Grade * course3Credit) + (course4Grade * course4Credit) + (course5Grade * course5Credit)) / (course1Credit + course2Credit + course3Credit + course4Credit + course5Credit);
+decimal rounded = Math.Round(gpa , 2);
+Console.WriteLine("Student: Sophia Johnson\n");
+Console.WriteLine("Course\t\t\tGrade\tCredit Hours");
+Console.WriteLine($"{course1Name}\t\t{course1Grade}\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t{course2Grade}\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t{course3Grade}\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t{course5Credit}\n");
+Console.Write($"Final GPA:\t\t{rounded}");
