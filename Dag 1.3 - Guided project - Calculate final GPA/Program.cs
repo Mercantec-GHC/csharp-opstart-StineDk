512c51fcdbbefb5051f19d6f1fdc88340b8358e3
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-19 16:08:23 +0100

    Update Program.cs

--- Dag 1.3 - Guided project - Calculate final GPA/Program.cs
+++ Dag 1.3 - Guided project - Calculate final GPA/Program.cs
@@ -20,8 +20,8 @@ int course5Grade = 4;
 decimal engSum = course1Grade * course1Credit;
 decimal algSum = course2Grade * course2Credit;
 decimal bioSum = course3Grade * course3Credit;
-decimal comSum = course4Grade* course4Credit;
-decimal psySum = course5Grade* course5Credit;
+decimal comSum = course4Grade * course4Credit;
+decimal psySum = course5Grade * course5Credit;
 
 decimal gpa = (((decimal)course1Grade * course1Credit) + (course2Grade * course2Credit) + (course3Grade * course3Credit) + (course4Grade * course4Credit) + (course5Grade * course5Credit)) / (course1Credit + course2Credit + course3Credit + course4Credit + course5Credit);
 decimal rounded = Math.Round(gpa , 2);
