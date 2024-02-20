2f6cc7a9e8d5788a1e07f3ad152b130a0d9d07df
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-20 10:58:53 +0100

    Update Program.cs

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -109,8 +109,11 @@ foreach (string name in names)
 
 int[] inventory = { 200, 450, 700, 175, 250 };
 int sum = 0;
+int bin = 0;
 foreach (int items in inventory)
 {
     sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
 }
 Console.WriteLine($"We have {sum} items in inventroy.");
