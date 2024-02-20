0db0cc5302d8bedcef38dec7a5f9932cdab0facf
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-20 09:58:54 +0100

    Update Program.cs

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -21,20 +21,24 @@ int largerValue = Math.Max(secondValue, firstValue);
 Console.WriteLine(largerValue);*/
 
 Random dice = new Random();
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+int roll1 = dice.Next(1, 2);
+int roll2 = dice.Next(1, 2);
+int roll3 = dice.Next(1, 2);
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-if ((roll1 == roll2) && (roll2 == roll3))
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled triples! +6 bonus points to total!");
-    total += 6;
-}
-else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    Console.WriteLine("You rolled doubles! +2 bonus points to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus points to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus points to total!");
+        total += 2;
+    }
 }
 if (total >= 15)
 {
