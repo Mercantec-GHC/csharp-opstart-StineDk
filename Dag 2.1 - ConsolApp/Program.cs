73397f4c27839f655caa9b8dfa0d7583bd4946fc
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-20 10:03:49 +0100

    Update Program.cs

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -21,9 +21,9 @@ int largerValue = Math.Max(secondValue, firstValue);
 Console.WriteLine(largerValue);*/
 
 Random dice = new Random();
-int roll1 = dice.Next(1, 2);
-int roll2 = dice.Next(1, 2);
-int roll3 = dice.Next(1, 2);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
@@ -40,13 +40,21 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
         total += 2;
     }
 }
-if (total >= 15)
+if (total >= 16)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
 }
 else
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a kitten!");
 }
 
 
