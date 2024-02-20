b8f0fab35ea6cfced5552487aef47302a6608b40
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-20 13:33:05 +0100

    Update Program.cs

--- Dag 2.2 - Guided Project/Program.cs
+++ Dag 2.2 - Guided Project/Program.cs
@@ -1,15 +1,16 @@
 ﻿//Starter koden
 // initialize variables - graded assignments 
-int currentAssignments = 5;
+int examAssignments = 5;
 
-string[] students = new string [] { "Sophia", "Andrew", "Emma", "Logan" };
+string[] students = new string [] { "Sophia", "Andrew", "Emma", "Logan", };
 
-int[] sophiaScores = { 90, 86, 87, 98, 100 };
-int[] andrewScores = { 92, 89, 81, 96, 90 };
-int[] emmaScores = { 90, 85, 87, 98, 68 };
-int[] loganScores = { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
 
 Console.WriteLine("Student\t\tGrade\n");
 
@@ -36,15 +37,78 @@ foreach (string name in students)
 
     int sumAssignmentScores = 0;
     decimal currentStudentGrade = 0;
+    int gradedAssignment = 0;
 
     foreach (int score in studentScores)
     {
-        sumAssignmentScores += score;
+        gradedAssignment += 1;
+
+        if (gradedAssignment <= examAssignments)
+        {
+            sumAssignmentScores += score;
+        }
+        else
+        {
+        sumAssignmentScores += score / 10;
+        }
     }
 
-    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+    if (currentStudentGrade >= 97)
+    {
+        currentStudentLetterGrade = "A+";
+    }
+    else if (currentStudentGrade >= 93)
+    {
+        currentStudentLetterGrade = "A";
+    }
+    else if (currentStudentGrade >= 90)
+    {
+        currentStudentLetterGrade = "A-";
+    }
+    else if (currentStudentGrade >= 87)
+    {
+        currentStudentLetterGrade = "B+";
+    }
+    else if (currentStudentGrade >= 83)
+    {
+        currentStudentLetterGrade = "B";
+    }
+    else if (currentStudentGrade >= 80)
+    {
+        currentStudentLetterGrade = "B-";
+    }
+    else if (currentStudentGrade >= 77)
+    {
+        currentStudentLetterGrade = "C+";
+    }
+    else if (currentStudentGrade >= 73)
+    {
+        currentStudentLetterGrade = "C";
+    }
+    else if (currentStudentGrade >= 70)
+    {
+        currentStudentLetterGrade = "C-";
+    }
+    else if (currentStudentGrade >= 67)
+    {
+        currentStudentLetterGrade = "D+";
+    }
+    else if (currentStudentGrade >= 63)
+    {
+        currentStudentLetterGrade = "D";
+    }
+    else if (currentStudentGrade >= 60)
+    {
+        currentStudentLetterGrade = "D-";
+    }
+    else
+    {
+        currentStudentLetterGrade = "´F";
+    }
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
 Console.WriteLine("\nPress the Enter key to continue");
