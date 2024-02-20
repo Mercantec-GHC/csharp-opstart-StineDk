d5c47b4932e12ed3373051a46a200cc2aacb1d44
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-20 12:35:00 +0100

    Guided Project update

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -107,7 +107,7 @@ foreach (string name in names)
     Console.WriteLine(name);
 }*/
 
-/*int[] inventory = { 200, 450, 700, 175, 250 };
+int[] inventory = { 200, 450, 700, 175, 250 };
 int sum = 0;
 int bin = 0;
 foreach (int items in inventory)
@@ -116,7 +116,7 @@ foreach (int items in inventory)
     bin++;
     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
 }
-Console.WriteLine($"We have {sum} items in inventroy.");*/
+Console.WriteLine($"We have {sum} items in inventroy.");
 
 /*string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 foreach (string orderID in orderIDs)
@@ -131,7 +131,7 @@ foreach (string orderID in orderIDs)
 Then it reverses the the array, aka the last item is now the first item.
 Then it count how many 'o' charecters there is in the array.
 Then it converts the array back in to a string, aka it writes the array items in there order.
-Then it prints out the answers on the console app.*/
+Then it prints out the answers on the console app.
 
 string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
@@ -145,5 +145,4 @@ foreach (char i in charMessage) { if (i == 'o') { x++; } }
 string new_message = new String(charMessage);
 
 Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
-
+Console.WriteLine($"'o' appears {x} times.");*/
