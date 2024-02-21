d95bc3f9c10f4808b2db78cdfeaefd14217d2445
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-21 10:31:09 +0100

    Update Program.cs

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -1,11 +1,106 @@
-﻿Console.WriteLine("a" == "a");
+﻿/*Console.WriteLine("a" == "a");
 Console.WriteLine("a" == "A");
 Console.WriteLine(1 == 2);
 
 string myValue = "a";
-Console.WriteLine(myValue ==  "a");
+Console.WriteLine(myValue ==  "a");*/
 
 
 /*string value1 = " a";
 string value2 = "A ";
 Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());*/
+
+
+/*Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");*/
+
+
+/*Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);*/
+
+
+/*string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));*/
+
+
+/*string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));*/
+
+
+/*string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));*/
+
+
+/*int a = 7;
+int b = 6;
+Console.WriteLine(a != b);
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2);*/
+
+
+/*int saleAmount = 1001;
+int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {discount}");*/
+
+
+/*int saleamount = 1001;
+Console.WriteLine($"Discount: {(saleamount > 1000 ? 100 : 50)}");*/
+
+
+
+
+/* Random number = new Random();
+int result = number.Next(1, 3);
+Console.WriteLine($"Coin landed on: {(result > 1 ? "Heads" : "Tails")}");
+
+Random coin = new Random();
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");*/
+
+
+/*string permission = "Admin|Manager";
+int level = 20;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+    Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else if (level <= 55)
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}*/
+
+
+bool flag = true;
+if (flag)
+{
+    int value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
