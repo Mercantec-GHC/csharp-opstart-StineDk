3c1bba4e4afdf8ed01964a2d8977c8bc38f2583f
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-21 14:57:17 +0100

    Update Program.cs

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -361,7 +361,7 @@ Console.WriteLine($"Your input value ({nummericChoice}) has been accepted.");*/
 
 
 
-string? readResult;
+/*string? readResult;
 string roleName = "";
 bool validEntry = false;
 
@@ -386,4 +386,42 @@ do
 } while (validEntry == false);
 
 Console.WriteLine($"Your input value ({roleName}) has been accepted.");
-readResult = Console.ReadLine();
+readResult = Console.ReadLine();*/
+
+
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
+    {
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
